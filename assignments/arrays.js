52df5a73f95e6ce15dd4b6b2639ba4586f834d46
lydiathornton <lydia.d.thornton@gmail.com>
2019-01-21 11:32:49 -0800

    finished objects.js plus objects stretch exercise

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -63,6 +63,12 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
+for (let i = 0; i < inventory; i ++) {
+    if (inventory[i][0] === 33) {
+        console.log(`Car 33 is a ${car_year} ${car_make} ${car_model}`)
+    }
+}
+
 console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
 
 
